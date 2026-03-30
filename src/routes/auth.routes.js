9e33c03b235cae404355041b9956d2ed43871215
Karabo Mashao <karabom@leantechnovations.com>
2026-03-30 17:07:50 +0200

    Adding the hosted URL

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,7 +9,7 @@ export const authHandler = ExpressAuth({
   secret: process.env.BETTER_AUTH_SECRET,
   callbacks: {
     async redirect({ url, baseUrl }) {
-      if (url.startsWith("http://localhost:5173")) return url
+      if (url.startsWith("https://thankful-ground-04f584e0f.4.azurestaticapps.net")) return url
       if (url.startsWith("/")) return `${baseUrl}${url}`
       return baseUrl
     },
